41f75c8a747957c6a193825ea35f6d0f8637c555
= <=>
2023-06-07 15:13:24 -0500

    Added an additional service to the TodoService port

--- src/ports/primaryPort.ts
+++ src/ports/primaryPort.ts
@@ -1,6 +1,7 @@
 import { ITask, ITodoList } from "../application/domain";
 
 export interface ITodoService {
+    createTodoList(list: ITodoList): ITodoList | undefined;
     getTodoList(id: number): ITodoList | undefined;
     addTask(todoListId: number, task: ITask): ITask | undefined;
     completeTask(task: ITask, todoListId: number): void;
